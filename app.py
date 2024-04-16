72976344c2d66698b852c0a39e3a310f30c09d08
Ian Chen <ianchen06@gmail.com>
2024-04-16 16:18:57 -0700

    add db

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+# Website for displaying, filting, sort and search
+# Books
--- quotes_scraper.py
+++ quotes_scraper.py
@@ -1,8 +1,18 @@
+import os
+
 import requests
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 
+from db import Database
+
+load_dotenv()
 BASE_URL = 'https://quotes.toscrape.com/page/{page}/'
+mydb = Database(os.getenv('DATABASE_URL'))
+mydb.create_table()
 
+# TODO: use argparse to enable truncating table
+mydb.truncate_table()
 
 quotes = []
 page = 1
@@ -24,9 +34,12 @@ while True:
         quote = {}
         quote['content'] = quote_div.select_one('span.text').text
         quote['author'] = quote_div.select_one('small.author').text
-        quote['author_link'] = quote_div.select('span')[1].select('a')[0]
-        quote['tags'] = [tag.text for tag in quote_div.select('a.tag')]
-        quote['tag_links'] = [tag['href'] for tag in quote_div.select('a.tag')]
+        #quote['author_link'] = quote_div.select('span')[1].select('a')[0]
+        quote['tags'] = ','.join([tag.text for tag in quote_div.select('a.tag')])
+        #quote['tag_links'] = [tag['href'] for tag in quote_div.select('a.tag')]
+        
+        # insert into database
+        mydb.insert_quote(quote)
         quotes.append(quote)
     page += 1
 
