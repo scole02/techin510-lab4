5f9bd6fa19425c8286b86c6a1024e4ec0779ccad
Ian Chen <ianchen06@gmail.com>
2024-04-17 22:34:34 -0700

    update db to use contextmanager

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
 import streamlit as st
+from dotenv import load_dotenv
+import os
 
-# Website for displaying, filting, sort and search
-# Books
+import pandas as pd
+
+from db import Database
+
+load_dotenv()
+
+with Database(os.getenv('DATABASE_URL')) as pg:
+    pg.create_table()
+
+    st.title('Quote Generator')
+
+    df = pd.read_sql('SELECT * FROM quotes', pg.con)
+    st.dataframe(df)
--- db.py
+++ db.py
@@ -6,6 +6,12 @@ class Database:
         self.con = psycopg2.connect(database_url)
         self.cur = self.con.cursor()
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.con.close()
+
     def create_table(self):
         q = """
         CREATE TABLE IF NOT EXISTS quotes (
